6bef159fe62e681fb03d2fab45bb4d9683e9b199
karlitoxz <electronicaysistemas@gmail.com>
2015-06-18 15:51:49 -0500

    Pagina original
    Primera Creacion

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>ElCatalogo</title>
+	<link rel="stylesheet" href="style/normalize.css">
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+	<header>
+		<div class="logo"><img src="img/elcatalogo_logo.png" alt="El_Catalogo_logo"></div>
+		<div class="catalogo"><h1>ELCÁTALOGO.COM.CO</h1></div>
+		<div class="buscador"><input type="text" class="textbox" size="30" placeholder="Buscar"></div>		
+		<div class="subir"><img src="img/BtnSubir.png" alt=""></div>
+		<div class="social">
+			<img src="img/facebook.png" alt="" width="30px" height="30px">
+			<img src="img/twitter.png" alt="" width="30px" height="30px">
+			<img src="img/youtube.png" alt="" width="30px" height="30px">
+		</div>
+	</header>
+
+<div class="separcion"></div>
+
+<nav></nav>
+
+<secion>
+	<article></article>
+	<article></article>
+	<article></article>
+	<article></article>
+	<article></article>
+	<article></article>
+</secion>
+
+<footer></footer>
+
+</body>
+</html>
